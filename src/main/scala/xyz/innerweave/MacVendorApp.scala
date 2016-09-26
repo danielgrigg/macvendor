4c5008082084c2faff4d2728bb48d74275c12bac
Daniel Casimir Grigg <daniel.grigg@skyfii.com>
2016-09-26 18:41:02 +1000

    Code tidying and render the search as json

--- src/main/scala/xyz/innerweave/MacVendorApp.scala
+++ src/main/scala/xyz/innerweave/MacVendorApp.scala
@@ -1,163 +1,69 @@
 package xyz.innerweave
 
-import java.nio.file.{Files, Path, Paths}
-
-import akka.actor.Status.Failure
-import akka.actor.{Actor, ActorSystem, Props}
+import akka.actor.{ActorSystem, Props}
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model._
-import akka.stream.scaladsl.{FileIO, Flow, Framing, Keep, Sink, Source}
-import akka.stream.{ActorMaterializer, IOResult}
-import akka.util.{ByteString, Timeout}
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import akka.pattern.{Backoff, BackoffSupervisor, ask}
+import akka.stream.ActorMaterializer
+import akka.util.Timeout
+import com.typesafe.config.ConfigFactory
 
-import scala.concurrent.Future
+import scala.concurrent.Await
 import scala.concurrent.duration._
-import scala.io.StdIn
-import scala.util.matching.Regex
-import akka.pattern.{Backoff, BackoffSupervisor, ask, pipe}
-
-import scala.util.Random
-
-case object OuiUpdate
-case class OuiGet(macPrefix: Int)
-case class OuiGetResponse(vendor: Option[String])
-case class OuiUpdateResult(cache: Map[Int, String])
-
-
-class OuiDb extends Actor with akka.actor.ActorLogging {
-  import context.dispatcher
-  final implicit val materializer = ActorMaterializer()
-
-  def receive = {
-    case OuiUpdate =>
-      val cachedDbFuture = if (shouldUpdate) {
-        Http(context.system).singleRequest(HttpRequest(uri = url))
-          .flatMap(r =>
-            r.entity.dataBytes
-              .via(Framing.delimiter(ByteString("\n"), maximumFrameLength = 1024, allowTruncation = true))
-              .map(_.utf8String)
-              .map(s => regex.findFirstMatchIn(s) match {
-                case Some(regex(prefix, fullName)) => s"$prefix\t$fullName\n"
-                case None => ""
-              })
-              .runWith(lineSink(dbPath))
-          ).map(_.wasSuccessful)
-      } else {
-        Future.successful(true)
-      }
-      cachedDbFuture.flatMap{result =>
-        FileIO.fromPath(dbPath)
-          .via(Framing.delimiter( ByteString("\n"), maximumFrameLength = 1024, allowTruncation = true))
-          .map(_.utf8String)
-          .runFold(Map[Int, String]()){ case (acc, s) =>
-            csvRegex.findFirstIn(s) match {
-              case None => acc
-              case Some(csvRegex(macStr, vendor)) =>
-                val mac = Integer.parseUnsignedInt(macStr, 16)
-                acc + (mac -> vendor)
-            }
-          }
-      }.map(OuiUpdateResult).pipeTo(self)
-
-    case OuiUpdateResult(newCache) =>
-      cache = newCache
-      println("updated")
-
-    case OuiGet(prefix) =>
-      sender() ! OuiGetResponse(cache.get(prefix))
-  }
-
-  var cache: Map[Int, String] = Map.empty
-
-  def lineSink(path: Path): Sink[String, Future[IOResult]] =
-    Flow[String]
-      .map(s => ByteString(s))
-      .toMat(FileIO.toPath(path))(Keep.right)
-
-  val name = "oui"
-  val dbPath = Paths.get(name + ".csv")
-  val regex = """([a-zA-Z0-9]+)[\s\t]*\(base 16\)[\s\t]*(.*)""".r
-  val csvRegex = """(.*)\t(.*)""".r
-  val url = "http://localhost:8080/oui.txt"
-
-  def shouldUpdate: Boolean = {
-    val oneWeek = 7L * 24L * 3600L * 1000L
-    !Files.exists(dbPath) ||
-      (DateTime.now.clicks - Files.getLastModifiedTime(dbPath).toMillis) > oneWeek
-  }
-}
-
-class FooActor extends Actor with akka.actor.ActorLogging {
-  import context.dispatcher
-
-  final implicit val materializer = ActorMaterializer()
-  val url = "http://localhost:8080/oui.txt"
-
-  override def postRestart(reason: Throwable) {
-    super.postRestart(reason)
-    log.info(s"Restarted because of ${reason.getMessage}")
-  }
-
-  override def preRestart(reason: Throwable, message: Option[Any]): Unit = {
-    super.preRestart(reason, message)
-    log.info(s"PreRestart because of ${reason.getMessage}")
-  }
-
-  override def preStart(): Unit = {
-    super.preStart()
-    log.info("Foo actor started")
-  }
-
-//  self ! OuiUpdate
-
-  def receive = {
-    case OuiUpdate =>
-      val f: Future[HttpResponse] = Http(context.system).singleRequest(HttpRequest(uri = url))
-      f.pipeTo(self)
-      println("OuiUpdate successful!")
-
-    case HttpResponse(status, headers, entity, protocol) =>
-      println("got response!")
-
-    case Failure(ex) =>
-      println("got exception: " + ex.getMessage)
-      throw ex
-  }
-}
 
 object MacVendorApp extends App {
 
   implicit val system = ActorSystem("MacVendor")
   implicit val materializer = ActorMaterializer()
   implicit val executionContext = system.dispatcher
-  implicit val timeout = Timeout(10.seconds)
+  implicit val timeout = Timeout(1.seconds)
 
-
-    val childProps = Props(classOf[FooActor])
-    val supervisorProps = BackoffSupervisor.props(
-      Backoff.onFailure(
-        childProps,
-        childName = "Foo1",
-        minBackoff = 2.seconds,
-        maxBackoff = 15.seconds,
-        randomFactor = 0.2))
-
-  val supervisor = system.actorOf(supervisorProps, "supervisor1")
-
-  while(true) {
-    StdIn.readLine()
-    supervisor ! OuiUpdate
-    println("update sent 1")
+  scala.sys.addShutdownHook {
+    finish()
   }
 
-//  val actor = system.actorOf(Props(new OuiDb))
-//  actor ! OuiUpdate
-//  StdIn.readLine()
-//  actor.ask(OuiGet(0xE043DB)).mapTo[OuiGetResponse].onComplete {
-//    case Success(r) => println(r)
-//    case Failure(ex) => println(ex.getMessage)
-//  }
+  val childProps = Props(classOf[OuiDbActor])
+  val supervisorProps = BackoffSupervisor.props(
+    Backoff.onFailure(
+      childProps,
+      childName = "OuiDb",
+      minBackoff = 3.seconds,
+      maxBackoff = 1.hour,
+      randomFactor = 0.2))
 
-  StdIn.readLine()
-  system.terminate()
+  val supervisor = system.actorOf(supervisorProps, "supervisor")
+
+  val route: Route =
+    path("vendor") {
+      get {
+        parameters('mac.as[String]) { (macStr) =>
+          val mac = Integer.parseUnsignedInt(macStr.replaceAll("[:-]", ""), 16)
+          onSuccess(supervisor.ask(OuiGet(mac))) {
+            case OuiGetResponse(Some(vendor)) =>
+              complete(s"""{"vendor":"$vendor"}""")
+            case OuiGetResponse(None) =>  ???
+            case _ => ???
+          }
+        }
+      }
+    }
+
+  val config = ConfigFactory.load()
+  val (iface, port) = (config.getString("http.interface"), config.getInt("http.port"))
+
+  Http().bindAndHandle(route, iface, port).onComplete {
+    case scala.util.Success(binding) =>
+      println(s"Server online at $iface:$port")
+    case scala.util.Failure(ex) =>
+      println(s"Failed to bind to $iface:$port: ${ex.getMessage}")
+      finish()
+  }
+
+  def finish() = {
+    println("Shutting down")
+    system.terminate()
+    Await.result(system.whenTerminated, 30.seconds)
+    println("Terminated")
+  }
 }
---
+++ src/main/scala/xyz/innerweave/OuiUpdateDb.scala
@@ -0,0 +1,114 @@
+package xyz.innerweave
+
+import java.nio.file.{Files, Path, Paths}
+
+import akka.actor.Actor
+import akka.actor.Status.Failure
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.pattern.pipe
+import akka.stream.scaladsl.{FileIO, Flow, Framing, Keep, Sink}
+import akka.stream.{ActorMaterializer, IOResult}
+import akka.util.ByteString
+
+import scala.concurrent.Future
+
+
+/**
+  * Request a refresh of the mac-vendor database
+  */
+case object OuiUpdate
+
+/**
+  * A refreshed cache
+  * @param cache
+  */
+case class OuiUpdateResult(cache: Map[Int, String])
+
+/**
+  * Request the vendor name for a mac prefix
+  * @param macPrefix a 3B vendor prefix, eg 0xAABBCC
+  */
+case class OuiGet(macPrefix: Int)
+
+/**
+  * The vendor name for a corresponding OuiGet request.
+  * @param vendor the Oui name
+  */
+case class OuiGetResponse(vendor: Option[String])
+
+
+class OuiDbActor extends Actor with akka.actor.ActorLogging {
+
+  import context.dispatcher
+
+  final implicit val materializer = ActorMaterializer()
+
+  def receive = {
+    case OuiUpdate =>
+      val updateResultFuture = for {
+        _ <- if (refreshDue) cacheOuiDbToDisk() else Future.successful(true)
+        newCache <- cacheOuiDbToMemory()
+      } yield OuiUpdateResult(newCache)
+      updateResultFuture.pipeTo(self)
+
+      // Re-throw any Failure updating the db
+    case Failure(ex) => throw ex
+
+    case OuiUpdateResult(newCache) => cache = newCache
+
+    case OuiGet(prefix) => sender() ! OuiGetResponse(cache.get(prefix))
+  }
+
+  var cache: Map[Int, String] = Map.empty
+  val diskCachePath = Paths.get("oui.csv")
+  val ouiRegex = """([a-zA-Z0-9]+)[\s\t]*\(base 16\)[\s\t]*(.*)""".r
+  val diskCacheRegex = """(.*)\t(.*)""".r
+  val sourceUrl = "http://localhost:8080/oui.txt"
+
+  def cacheOuiDbToMemory() = {
+    FileIO.fromPath(diskCachePath)
+      .via(Framing.delimiter(ByteString("\n"), maximumFrameLength = 1024, allowTruncation = true))
+      .map(_.utf8String)
+      .runFold(Map[Int, String]()) { case (acc, s) =>
+        diskCacheRegex.findFirstIn(s) match {
+          case None => acc
+          case Some(diskCacheRegex(macStr, vendor)) =>
+            val mac = Integer.parseUnsignedInt(macStr, 16)
+            acc + (mac -> vendor)
+        }
+      }
+  }
+
+  def cacheOuiDbToDisk(): Future[Boolean] = {
+    Http(context.system).singleRequest(HttpRequest(uri = sourceUrl))
+      .flatMap(r =>
+        r.entity.dataBytes
+          .via(Framing.delimiter(ByteString("\n"), maximumFrameLength = 1024, allowTruncation = true))
+          .map(_.utf8String)
+          .map(s => ouiRegex.findFirstMatchIn(s) match {
+            case Some(ouiRegex(prefix, fullName)) => s"$prefix\t$fullName\n"
+            case None => ""
+          })
+          .runWith(lineSink(diskCachePath))
+      ).map(_.wasSuccessful)
+  }
+
+  def lineSink(path: Path): Sink[String, Future[IOResult]] =
+    Flow[String]
+      .map(s => ByteString(s))
+      .toMat(FileIO.toPath(path))(Keep.right)
+
+  /**
+    * Refresh if the disk cache doesn't exist or is older than a week.
+    */
+  def refreshDue: Boolean = {
+    val oneWeekMillis = 7L * 24L * 3600L * 1000L
+    val ageMillis = DateTime.now.clicks - Files.getLastModifiedTime(diskCachePath).toMillis
+    !Files.exists(diskCachePath) || ageMillis > oneWeekMillis
+  }
+
+  // Ensure the cache is refreshed when the actor starts
+  self ! OuiUpdate
+}
+
